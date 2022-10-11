232b8c97aae248e0d177fb82543a4394391c0333
moridonman <kanta.80721.24@gmail.com>
2022-10-11 21:54:49 +0900

    first vue

---
+++ src/views/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <TestComponent />
+    </div>
+</template>
+
+<script>
+import TestComponent from '@/components/TestComponent.vue'
+export default {
+    data() {
+        return {
+            title: "初めてのVue.jsアプリです！",
+            subtitle: "ようこそ",
+            isEnabled: true
+        };
+    },
+    computed: {
+        text() {
+            if (this.isEnabled) {
+                return "こんにちは！";
+            }
+            else {
+                return "さよなら！";
+            }
+        }
+    },
+    methods: {
+        toggle() {
+            this.isEnabled = !this.isEnabled;
+        }
+    },
+    components: { TestComponent }
+}
+</script>
